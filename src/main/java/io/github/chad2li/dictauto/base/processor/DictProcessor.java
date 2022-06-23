1e41c7807065fb7a5fa8cf3706dc4f7e6ae29714
Chad <li17206@163.com>
2022-06-23 23:51:39 +0800

    done setter&getter

--- src/main/java/io/github/chad2li/dictauto/base/processor/DictProcessor.java
+++ src/main/java/io/github/chad2li/dictauto/base/processor/DictProcessor.java
@@ -1,16 +1,19 @@
 package io.github.chad2li.dictauto.base.processor;
 
 import cn.hutool.core.collection.CollectionUtil;
+import cn.hutool.core.exceptions.ExceptionUtil;
 import com.sun.source.tree.Tree;
 import com.sun.source.util.TreePath;
 import com.sun.tools.javac.api.JavacTrees;
 import com.sun.tools.javac.code.Flags;
+import com.sun.tools.javac.code.TypeTag;
 import com.sun.tools.javac.processing.JavacProcessingEnvironment;
 import com.sun.tools.javac.tree.JCTree;
 import com.sun.tools.javac.tree.TreeMaker;
 import com.sun.tools.javac.tree.TreeTranslator;
 import com.sun.tools.javac.util.Context;
 import com.sun.tools.javac.util.List;
+import com.sun.tools.javac.util.ListBuffer;
 import com.sun.tools.javac.util.Names;
 import io.github.chad2li.dictauto.base.annotation.DictId;
 import io.github.chad2li.dictauto.base.cst.DictCst;
@@ -109,7 +112,8 @@ public class DictProcessor extends AbstractProcessor {
     private void tryProcess4(Set<? extends TypeElement> annotations, RoundEnvironment env) {
         Set<? extends Element> dicts = env.getElementsAnnotatedWith(DictId.class);
         Log.write("process size: " + dicts.size());
-        dictFor: for (Element dictElement : dicts) {
+        dictFor:
+        for (Element dictElement : dicts) {
             String varName = dictElement.getSimpleName().toString();
             String varPrefix = varName.substring(0, varName.indexOf(DictCst.FIELD_DICT_ID_SUFFIX));
             TypeElement clsElt = (TypeElement) dictElement.getEnclosingElement();
@@ -130,11 +134,15 @@ public class DictProcessor extends AbstractProcessor {
                     }
                 }
             }
+
+            String methodName = dictItemFieldName.substring(0, 1).toUpperCase() + dictItemFieldName.substring(1);
+            String getter = "get" + methodName;
+            String setter = "set" + methodName;
             Log.write("\t\tadd    " + clsName + "." + dictItemFieldName);
             tree.accept(new TreeTranslator() {
                 @Override
                 public void visitClassDef(JCTree.JCClassDecl jcClassDecl) {
-
+                    // import
                     addImportInfo(clsElt);
 
                     // todo 处理 list 类型
@@ -145,7 +153,14 @@ public class DictProcessor extends AbstractProcessor {
                             , null
                     );
 
-                    jcClassDecl.defs = jcClassDecl.defs.append(dictDecl);
+                    JCTree.JCMethodDecl setterMethod = setter(dictDecl, setter);
+                    JCTree.JCMethodDecl getterMethod = getter(dictDecl, getter);
+
+                    jcClassDecl.defs = jcClassDecl.defs
+                            .append(dictDecl)
+                            .append(setterMethod)
+                            .append(getterMethod)
+                    ;
 
                     Log.write("\t\tadd    " + clsName + "." + dictItemFieldName + " succ");
                     super.visitClassDef(jcClassDecl);
@@ -155,7 +170,69 @@ public class DictProcessor extends AbstractProcessor {
     }
 
     /**
-     * 给{@code class}文件增加 {@code import} 语句
+     * 生成 dictDecl 属性的 setter方法
+     *
+     * @param dictDecl   属性
+     * @param setterName 完整的setter方法名
+     * @return dictDecl属性的setter方法
+     * @date 2022/6/23 22:51
+     * @author chad
+     * @since 1 by chad at 2022/6/23
+     */
+    private JCTree.JCMethodDecl setter(JCTree.JCVariableDecl dictDecl, String setterName) {
+        ListBuffer<JCTree.JCStatement> statements = new ListBuffer<>();
+        //生成函数体 this.name = name;
+        statements.append(treeMaker.Exec(treeMaker.Assign(treeMaker.Select(treeMaker.Ident(names.fromString("this")),
+                dictDecl.name), treeMaker.Ident(dictDecl.name))));
+        JCTree.JCBlock body = treeMaker.Block(0, statements.toList());
+        JCTree.JCVariableDecl paramVal = treeMaker.VarDef(
+                treeMaker.Modifiers(Flags.PARAMETER), //访问标志
+                dictDecl.name,
+                dictDecl.vartype,
+                //初始化语句
+                null
+        );
+        //生成方法
+        return treeMaker.MethodDef(
+                treeMaker.Modifiers(Flags.PUBLIC),
+                DictProcessor.this.names.fromString(setterName),
+                //返回类型
+                treeMaker.TypeIdent(TypeTag.VOID),
+                //泛型形参列表
+                List.nil(),
+                //参数列表
+                List.of(paramVal),
+                //异常列表
+                List.nil(),
+                //方法体
+                body,
+                //默认方法（可能是interface中的那个default）
+                null
+        );
+    }
+
+    /**
+     * 生成 dictDecl 的 getter 方法
+     *
+     * @param dictDecl   属性
+     * @param getterName 完整的方法名
+     * @return getter 方法
+     * @date 2022/6/23 22:44
+     * @author chad
+     * @since 1 by chad at 2022/6/23
+     */
+    private JCTree.JCMethodDecl getter(JCTree.JCVariableDecl dictDecl, String getterName) {
+        ListBuffer<JCTree.JCStatement> statements = new ListBuffer<>();
+        //生成函数体 return this.字段名
+        statements.append(treeMaker.Return(treeMaker.Select(treeMaker.Ident(names.fromString("this")), dictDecl.getName())));
+        JCTree.JCBlock body = treeMaker.Block(0, statements.toList());
+        //生成方法
+        return treeMaker.MethodDef(treeMaker.Modifiers(Flags.PUBLIC), DictProcessor.this.names.fromString(getterName)
+                , dictDecl.vartype, List.nil(), List.nil(), List.nil(), body, null);
+    }
+
+    /**
+     * 给 class 文件增加 import 语句
      *
      * @param element class
      * @date 2022/5/19 12:19
@@ -165,33 +242,39 @@ public class DictProcessor extends AbstractProcessor {
     private void addImportInfo(Element element) {
         TreePath treePath = javacTrees.getPath(element);
         Tree leaf = treePath.getLeaf();
-        if (treePath.getCompilationUnit() instanceof JCTree.JCCompilationUnit && leaf instanceof JCTree) {
-            JCTree.JCCompilationUnit jccu = (JCTree.JCCompilationUnit) treePath.getCompilationUnit();
-
-            for (JCTree jcTree : jccu.getImports()) {
-                if (jcTree != null && jcTree instanceof JCTree.JCImport) {
-                    JCTree.JCImport jcImport = (JCTree.JCImport) jcTree;
-                    if (jcImport.qualid != null && jcImport.qualid instanceof JCTree.JCFieldAccess) {
-                        JCTree.JCFieldAccess jcFieldAccess = (JCTree.JCFieldAccess) jcImport.qualid;
-                        try {
-                            if (TRACKER_PACKAGE.equals(jcFieldAccess.selected.toString()) && TRACKER_CLASS.equals(jcFieldAccess.name.toString())) {
-                                return;
-                            }
-                        } catch (NullPointerException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                }
-            }
-            java.util.List<JCTree> trees = new ArrayList<>();
-            trees.addAll(jccu.defs);
-            JCTree.JCIdent ident = treeMaker.Ident(names.fromString(TRACKER_PACKAGE));
-            JCTree.JCImport jcImport = treeMaker.Import(treeMaker.Select(
-                    ident, names.fromString(TRACKER_CLASS)), false);
-            if (!trees.contains(jcImport)) {
-                trees.add(0, jcImport);
-            }
-            jccu.defs = List.from(trees);
+        if (!(treePath.getCompilationUnit() instanceof JCTree.JCCompilationUnit) || !(leaf instanceof JCTree)) {
+            return;
         }
+        JCTree.JCCompilationUnit jccu = (JCTree.JCCompilationUnit) treePath.getCompilationUnit();
+
+        for (JCTree jcTree : jccu.getImports()) {
+            if (null == jcTree || !(jcTree instanceof JCTree.JCImport)) {
+                continue;
+            }
+            JCTree.JCImport jcImport = (JCTree.JCImport) jcTree;
+            if (null == jcImport.qualid || !(jcImport.qualid instanceof JCTree.JCFieldAccess)) {
+                continue;
+            }
+            JCTree.JCFieldAccess jcFieldAccess = (JCTree.JCFieldAccess) jcImport.qualid;
+            try {
+                if (TRACKER_PACKAGE.equals(jcFieldAccess.selected.toString()) && TRACKER_CLASS.equals(jcFieldAccess.name.toString())) {
+                    // 已存在的 import，不重复导入
+                    return;
+                }
+            } catch (NullPointerException e) {
+                Log.write(ExceptionUtil.stacktraceToString(e));
+            }
+        }
+
+        java.util.List<JCTree> trees = new ArrayList<>();
+        trees.addAll(jccu.defs);
+        JCTree.JCIdent ident = treeMaker.Ident(names.fromString(TRACKER_PACKAGE));
+        JCTree.JCImport jcImport = treeMaker.Import(treeMaker.Select(
+                ident, names.fromString(TRACKER_CLASS)), false);
+        if (!trees.contains(jcImport)) {
+            trees.add(0, jcImport);
+        }
+
+        jccu.defs = List.from(trees);
     }
 }
