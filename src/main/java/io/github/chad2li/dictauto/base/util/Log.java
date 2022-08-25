9aa48475e9505d51eb16f3c8fa6a41a5231628f9
Chad <li17206@163.com>
2022-08-25 17:42:57 +0800

    log

--- src/main/java/io/github/chad2li/dictauto/base/util/Log.java
+++ src/main/java/io/github/chad2li/dictauto/base/util/Log.java
@@ -6,9 +6,6 @@ import cn.hutool.core.util.ArrayUtil;
 import javax.annotation.processing.Messager;
 import javax.tools.Diagnostic;
 import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.util.StringJoiner;
 
 /**
@@ -23,46 +20,9 @@ public class Log {
     private static Messager messager;
 
     private static final String TAG = "DictAuto: ";
-    /**
-     * 是否允许调试
-     */
-    private static final boolean IS_DEBUG = false;
 
     public static void init(Messager messager) {
         Log.messager = messager;
-
-        initWriter();
-    }
-
-    private static void initWriter() {
-        if (!IS_DEBUG) {
-            // 未开启调试
-            return;
-        }
-        if (null != Log.WRITER) {
-            return;
-        }
-        synchronized (Log.class) {
-            if (null != Log.WRITER) {
-                return;
-            }
-        }
-        // todo jar目录
-        String name = Log.class.getClassLoader().getResource(".").toString();
-        name += "/processor/" + System.currentTimeMillis() + ".log";
-//        name = "D:\\tmp\\processor\\" + System.currentTimeMillis() + ".log";
-        name = "/tmp/processor/" + System.currentTimeMillis() + ".log";
-        File file = new File(name);
-        try {
-            if (!file.getParentFile().isDirectory()) {
-                file.getParentFile().mkdirs();
-            }
-            file.createNewFile();
-            WRITER = new BufferedWriter(new FileWriter(file));
-        } catch (IOException e) {
-            Log.messager.printMessage(Diagnostic.Kind.WARNING, ExceptionUtil.stacktraceToString(e));
-            throw new RuntimeException(e);
-        }
     }
 
     /**
@@ -74,25 +34,14 @@ public class Log {
      * @since 1 by chad at 2022/5/19
      */
     public static void write(String... msg) {
-        if (!IS_DEBUG) {
-            // 未开启调试
-            return;
-        }
         if (ArrayUtil.isEmpty(msg)) {
             return;
         }
-        try {
-            StringJoiner sj = new StringJoiner("\n");
-            for (String s : msg) {
-                sj.add(TAG + s);
-            }
-            WRITER.write(sj.toString());
-            WRITER.flush();
-            Log.messager.printMessage(Diagnostic.Kind.NOTE, sj.toString());
-        } catch (IOException e) {
-            Log.messager.printMessage(Diagnostic.Kind.WARNING, ExceptionUtil.stacktraceToString(e));
-            throw new RuntimeException(e);
+        StringJoiner sj = new StringJoiner("\n");
+        for (String s : msg) {
+            sj.add(TAG + s);
         }
+        Log.messager.printMessage(Diagnostic.Kind.NOTE, sj.toString());
     }
 
     /**
